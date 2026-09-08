cd930dff83f580bc6c9fc323b98767845cce269d
yanwowwwang119-cpu <yanwowwwang119@gmail.com>
2026-09-08 17:36:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ SLOT_LIST: otherCourseSlots,
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oI6z32-thaOTxjmHmWkg2R8ZkWE0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	USxuV1qBGdD71aE8M18tlClqNVpsbLlG73eeCkpvnEw',
+      useTemplateId: 'USxuV1qBGdD71aE8M18tlClqNVpsbLlG73eeCkpvnEw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-21',
       festivals: [
