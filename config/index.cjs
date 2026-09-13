6bd44e849d4527adde89c9f8510d72531906ef8d
yanwowwwang119-cpu <yanwowwwang119@gmail.com>
2026-09-14 00:47:39 +0800

    Update WeChat template ID in configuration

--- config/index.cjs
+++ config/index.cjs
@@ -102,7 +102,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oI6z32-thaOTxjmHmWkg2R8ZkWE0',
       // 使用微信测试号：安排模板ID
-      useTemplateId: 'uWpFGmZHn9vb00TML5DXt7PVhb_U8aqAlFslIIoWiJM',
+      useTemplateId: 'wxb_K-s2m3jzc_94LvrQyLfl-WsxFgxEPl6grAQ0o8s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-21',
       festivals: [
