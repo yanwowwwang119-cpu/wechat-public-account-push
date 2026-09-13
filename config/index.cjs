850b661d04085111d53f26c0dfd277ac6b3963c7
yanwowwwang119-cpu <yanwowwwang119@gmail.com>
2026-09-13 23:49:08 +0800

    Change WeChat template ID in config
    Updated WeChat template ID for notifications.

--- config/index.cjs
+++ config/index.cjs
@@ -99,7 +99,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oI6z32-thaOTxjmHmWkg2R8ZkWE0',
       // 使用微信测试号：安排模板ID
-      useTemplateId: 'uWpFGmZHn9vb00TML5DXt7PVhb_U8aqAlFslIIoWiJM',
+      useTemplateId: 'USxuV1qBGdD71aE8M18tlClqNVpsbLlG73eeCkpvnEw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-21',
       festivals: [
